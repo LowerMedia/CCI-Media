2251952f11df65e0c94e74f2ce26ce101b21c15e
hawkeye126 <pete.lower@gmail.com>
2014-08-17 11:36:45 -0700

    add featured post img in non duplicatable loop in order to move secondary before primary

--- sidebar.php
+++ sidebar.php
@@ -12,13 +12,33 @@
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div id="secondary" class="widget-area" role="complementary">
+			
 			<?php if ( get_header_image() ) : ?>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
 			<?php endif; ?>
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-			<?php $posts = get_posts('category=featured-magazine&numberposts=1'); foreach($posts as $post) { ?>
+
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>			
+
+			<?php $my_query = new WP_Query( 'category_name=featured-magazine&posts_per_page=1' );
+			while ( $my_query->have_posts() ) : $my_query->the_post();
+			$do_not_duplicate = $post->ID; ?>
 				<h2><?php the_title(); ?></h2><br />
 				<a href="http://cabinetmakerfdm.com/" target="_blank"><?php the_post_thumbnail( 'medium' ); ?></a>
-			<?php } ?>
+			<?php endwhile; ?>
+
 		</div><!-- #secondary -->
 	<?php endif; ?>
+
+	<?php /*
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php  $posts = get_posts('category=featured-magazine&numberposts=1'); 
+				foreach($posts as $post) { ?>
+					<h2><?php the_title(); ?></h2><br />
+					<a href="http://cabinetmakerfdm.com/" target="_blank"><?php the_post_thumbnail( 'medium' ); ?></a>
+				<?php }?>
+
+			<?php endwhile; else : ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
+			*/
